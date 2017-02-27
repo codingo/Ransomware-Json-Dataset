83c8fd633b4eb6d4ad5c6a1b47b0be5a7c8946af
Michael <codingo@outlook.com>
2017-02-28 09:21:38 +1000

    Base files for ransomware dataset generation

---
+++ GoogleSpreadsheetToJson.py
@@ -0,0 +1,9 @@
+'''
+AUTHOR: Michael Skelton
+DATE: 28 February 2017
+
+CREDIT: Generates a json dataset from the public Google Spreadsheet originally created by
+Mosh (@nyxbone) and @cyb3rops.
+
+See: http://goo.gl/b9R8DE
+'''
