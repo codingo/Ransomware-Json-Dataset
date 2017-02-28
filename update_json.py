a4c62b66155354f1eaddf2eb70df7b8012a83ff6
Michael <codingo@outlook.com>
2017-02-28 15:02:08 +1000

    Updated project structure and modules

--- update_json.py
+++ update_json.py
@@ -9,21 +9,15 @@ See: http://goo.gl/b9R8DE
 '''
 
 
-import urllib.request
 import pandas as pd
-from prettyJSON import formatInput as formatJson
+from pretty_json import formatInput as formatJson
+from download_file import download_file
 
 SOURCESHEET = 'https://docs.google.com/spreadsheets/d/1TWS238xacAto-fLKh1n5uTsdijWdCEsGIM0Y0Hvmc5g/pub?output=xlsx'
 OUTPUTSHEET = 'RansomwareOverview.xlsx'
 JSONFILE = 'RansomwareOverview.json'
 
-# download and save ransomware overview file locally
-try:
-    urllib.request.urlretrieve(SOURCESHEET, OUTPUTSHEET)
-except IOError:
-    print('An error occured trying to write an updated spreadsheet. Do you already have it open?')
-except urllib.error.URLError:
-    print('An error occured trying to download the file. Please check the source and try again.')
+download_file(SOURCESHEET, OUTPUTSHEET)
 
 sheet = pd.read_excel(open(OUTPUTSHEET,'rb'), sheetname='Ransomware')
 
