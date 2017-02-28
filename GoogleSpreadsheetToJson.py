ba86f972109e08e49df75870a6c777cdbfa6a19b
Michael <codingo@outlook.com>
2017-02-28 10:48:32 +1000

    Base library from xlrd to pandas for better unicode support

--- GoogleSpreadsheetToJson.py
+++ GoogleSpreadsheetToJson.py
@@ -8,8 +8,9 @@ Mosh (@nyxbone) and @cyb3rops.
 See: http://goo.gl/b9R8DE
 '''
 
+
 import urllib.request
-from xlrd import open_workbook
+import pandas as pd
 
 SOURCESHEET = 'https://docs.google.com/spreadsheets/d/1TWS238xacAto-fLKh1n5uTsdijWdCEsGIM0Y0Hvmc5g/pub?output=xlsx'
 WORKBOOK = 'RansomwareOverview.xlsx'
@@ -22,36 +23,5 @@ except IOError:
 except urllib.error.URLError:
     print('An error occured trying to download the file. Please check the source and try again')
 
-book = open_workbook(WORKBOOK, on_demand=True)
-print("The number of worksheets is {0}".format(book.nsheets))
-print("Worksheet name(s): {0}".format(book.sheet_names()))
-
-sheet_names = book.sheet_names()
-print('Sheet Names', sheet_names)
-
-xl_sheet = book.sheet_by_name(sheet_names[0])
-print ('Opened sheet: %s' % xl_sheet.name)
-
-# Pull the first row by index
-#  (rows/columns are also zero-indexed)
-#
-row = xl_sheet.row(0)  # 1st row
-
-# Print 1st row values and types
-#
-from xlrd.sheet import ctype_text  
-
-print('(Column #) type:value')
-for idx, cell_obj in enumerate(row):
-    cell_type_str = ctype_text.get(cell_obj.ctype, 'unknown type')
-    print('(%s) %s %s' % (idx, cell_type_str, cell_obj.value))
-
-# Print all values, iterating through rows and columns
-#
-num_cols = xl_sheet.ncols   # Number of columns
-for row_idx in range(0, xl_sheet.nrows):    # Iterate through rows
-    print ('-'*40)
-    print ('Row: %s' % row_idx)   # Print row number
-    for col_idx in range(0, num_cols):  # Iterate through columns
-        cell_obj = xl_sheet.cell(row_idx, col_idx)  # Get cell object by row, col
-        print ('Column: [%s] cell_obj: [%s]' % (col_idx, cell_obj))
+sheet = pd.read_excel(open(WORKBOOK,'rb'), sheetname='Ransomware')
+print(sheet)
