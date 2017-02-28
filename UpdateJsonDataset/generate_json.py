f4118cf19ee496dbabbe015344bdc247a83a9b2c
Michael <codingo@outlook.com>
2017-02-28 16:38:24 +1000

    Restuctured project and dependancies

---
+++ UpdateJsonDataset/generate_json.py
@@ -0,0 +1,34 @@
+import simplejson as json
+import xlrd
+from collections import OrderedDict
+
+def formatJson(input):
+    return json.dumps(json.loads(input), indent=4)
+
+def excel_to_json(filename):
+    wb = xlrd.open_workbook(filename)
+    sh = wb.sheet_by_index(0)
+
+    # List to hold dictionaries
+    c_list = []
+
+    # Iterate through each row in worksheet and fetch values into dict
+    for rownum in range(1, sh.nrows):
+        cars = OrderedDict()
+        row_values = sh.row_values(rownum)
+        cars['RansomwareName'] = row_values[0]
+        cars['KnownExtensions'] = row_values[1]
+        cars['KnownPatterns'] = row_values[2]
+        cars['RansomNoteFilenames'] = row_values[3]
+        cars['Comment'] = row_values[4]
+        cars['EncryptionAlgorithm'] = row_values[5]
+        cars['AlsoKnownAs'] = row_values[6]
+        cars['Decryptor'] = row_values[7]
+        cars['AdditionalInfo1'] = row_values[8]
+        cars['AdditionalInfo2'] = row_values[9]
+        cars['Screenshots'] = row_values[10]
+
+        c_list.append(cars)
+
+    # Serialize the list of dicts to JSON
+    return formatJson(json.dumps(c_list))
--- UpdateJsonDataset/update_json.py
+++ UpdateJsonDataset/update_json.py
@@ -8,12 +8,7 @@ Mosh (@nyxbone) and @cyb3rops.
 See: http://goo.gl/b9R8DE
 '''
 
-import pandas as pd
-import simplejson as json
-import xlrd
-from collections import OrderedDict
-
-from pretty_json import formatInput as formatJson
+from generate_json import excel_to_json
 from download_file import download_file
 
 SOURCESHEET = 'https://docs.google.com/spreadsheets/d/1TWS238xacAto-fLKh1n5uTsdijWdCEsGIM0Y0Hvmc5g/pub?output=xlsx'
@@ -24,33 +19,7 @@ def write_json_file(input, filename):
     output = open(filename, 'w')
     output.writelines(input)
 
-def excel_to_json(filename):
-    wb = xlrd.open_workbook(OUTPUTSHEET)
-    sh = wb.sheet_by_index(0)
 
-    # List to hold dictionaries
-    c_list = []
-
-    # Iterate through each row in worksheet and fetch values into dict
-    for rownum in range(1, sh.nrows):
-        cars = OrderedDict()
-        row_values = sh.row_values(rownum)
-        cars['RansomwareName'] = row_values[0]
-        cars['KnownExtensions'] = row_values[1]
-        cars['KnownPatterns'] = row_values[2]
-        cars['RansomNoteFilenames'] = row_values[3]
-        cars['Comment'] = row_values[4]
-        cars['EncryptionAlgorithm'] = row_values[5]
-        cars['AlsoKnownAs'] = row_values[6]
-        cars['Decryptor'] = row_values[7]
-        cars['AdditionalInfo1'] = row_values[8]
-        cars['AdditionalInfo2'] = row_values[9]
-        cars['Screenshots'] = row_values[10]
-
-        c_list.append(cars)
-
-    # Serialize the list of dicts to JSON
-    return formatJson(json.dumps(c_list))
 
 def generate_json(source_file, download_destination, json_file):
     download_file(source_file, download_destination)
