629d4aa3234bce681a85126916ce04018011e1cf
Michael <codingo@outlook.com>
2017-02-28 10:16:05 +1000

    Successful HTTP request to download file and open workbook

--- GoogleSpreadsheetToJson.py
+++ GoogleSpreadsheetToJson.py
@@ -9,9 +9,19 @@ See: http://goo.gl/b9R8DE
 '''
 
 import urllib.request
+from xlrd import open_workbook
 
-SPREADSHEET = 'https://docs.google.com/spreadsheets/d/1TWS238xacAto-fLKh1n5uTsdijWdCEsGIM0Y0Hvmc5g/pub?output=xlsx'
-OUTPUTFILE = 'RansomwareOverview.xlsx'
+SOURCESHEET = 'https://docs.google.com/spreadsheets/d/1TWS238xacAto-fLKh1n5uTsdijWdCEsGIM0Y0Hvmc5g/pub?output=xlsx'
+WORKBOOK = 'RansomwareOverview.xlsx'
 
 # download and save ransomware overview file locally
-urllib.request.urlretrieve(SPREADSHEET, OUTPUTFILE)
+try:
+    urllib.request.urlretrieve(SOURCESHEET, WORKBOOK)
+except IOError:
+    print('An error occured trying to write an updated spreadsheet. Do you already have it open?')
+except urllib.error.URLError:
+    print('An error occured trying to download the file. Please check the source and try again')
+
+book = open_workbook(WORKBOOK, on_demand=True)
+print("The number of worksheets is {0}".format(book.nsheets))
+print("Worksheet name(s): {0}".format(book.sheet_names()))
