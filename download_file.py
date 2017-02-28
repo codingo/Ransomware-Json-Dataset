86c80010cc8f2b3c0e9e1034526ed709a56f07ed
Michael <m.skelton@outlook.com>
2017-02-28 15:41:09 +1000

    Delete download_file.py

--- download_file.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-def download_file(source, destination):
-    try:
-        urllib.request.urlretrieve(source, destination)
-    except IOError:
-        print('An error occured trying to write an updated spreadsheet. Do you already have it open?')
-    except urllib.error.URLError:
-        print('An error occured trying to download the file. Please check the source and try again.')
