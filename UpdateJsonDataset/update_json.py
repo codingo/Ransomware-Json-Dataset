0a6ccfdf967b5f87deffe1c83475d7475682c791
Michael <codingo@outlook.com>
2017-02-28 16:26:18 +1000

    JSON structure change into object-format

--- UpdateJsonDataset/update_json.py
+++ UpdateJsonDataset/update_json.py
@@ -9,6 +9,10 @@ See: http://goo.gl/b9R8DE
 '''
 
 import pandas as pd
+import simplejson as json
+import xlrd
+from collections import OrderedDict
+
 from pretty_json import formatInput as formatJson
 from download_file import download_file
 
@@ -18,10 +22,35 @@ JSONFILE = '../RansomwareOverview.json'
 
 def write_json_file(input, filename):
     output = open(filename, 'w')
-    output.writelines(formatJson(input))
+    output.writelines(input)
 
 def excel_to_json(filename):
-    return pd.read_excel(open(filename,'rb'), sheetname='Ransomware').to_json()
+    wb = xlrd.open_workbook(OUTPUTSHEET)
+    sh = wb.sheet_by_index(0)
+
+    # List to hold dictionaries
+    c_list = []
+
+    # Iterate through each row in worksheet and fetch values into dict
+    for rownum in range(1, sh.nrows):
+        cars = OrderedDict()
+        row_values = sh.row_values(rownum)
+        cars['RansomwareName'] = row_values[0]
+        cars['KnownExtensions'] = row_values[1]
+        cars['KnownPatterns'] = row_values[2]
+        cars['RansomNoteFilenames'] = row_values[3]
+        cars['Comment'] = row_values[4]
+        cars['EncryptionAlgorithm'] = row_values[5]
+        cars['AlsoKnownAs'] = row_values[6]
+        cars['Decryptor'] = row_values[7]
+        cars['AdditionalInfo1'] = row_values[8]
+        cars['AdditionalInfo2'] = row_values[9]
+        cars['Screenshots'] = row_values[10]
+
+        c_list.append(cars)
+
+    # Serialize the list of dicts to JSON
+    return formatJson(json.dumps(c_list))
 
 def generate_json(source_file, download_destination, json_file):
     download_file(source_file, download_destination)
