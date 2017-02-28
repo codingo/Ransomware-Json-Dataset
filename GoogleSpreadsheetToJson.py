eafe5da72cf29b8f7f9f6565cc3a023cd3a5a978
Michael <codingo@outlook.com>
2017-02-28 10:03:00 +1000

    Download and save latest version of the ransomware overview spreadsheet

--- GoogleSpreadsheetToJson.py
+++ GoogleSpreadsheetToJson.py
@@ -7,3 +7,11 @@ Mosh (@nyxbone) and @cyb3rops.
 
 See: http://goo.gl/b9R8DE
 '''
+
+import urllib.request
+
+SPREADSHEET = 'https://docs.google.com/spreadsheets/d/1TWS238xacAto-fLKh1n5uTsdijWdCEsGIM0Y0Hvmc5g/pub?output=xlsx'
+OUTPUTFILE = 'RansomwareOverview.xlsx'
+
+# download and save ransomware overview file locally
+urllib.request.urlretrieve(SPREADSHEET, OUTPUTFILE)
